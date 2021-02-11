1d7ccd86c54bdd7774cbf9735822f46db9fc2e3b
rebeca-august <rebeca.cazacu@gmail.com>
2021-02-11 20:59:44 +0000

    finish Home, About and Error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A beatae, qui
+        ipsa illum, explicabo id sapiente quam deserunt alias veritatis est
+        autem, cum quisquam? Omnis corrupti optio, doloremque rem odit aliquid
+        incidunt itaque veniam, est dolorum nostrum iure, veritatis dignissimos.
+      </p>
+    </section>
   )
 }
 
